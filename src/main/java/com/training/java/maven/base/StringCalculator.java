2c4486cd3fb02f8c93bb9896b0a24c20470cafb4
semih.cavdar <semih.cavdar@trendyol.com>
2022-06-08 16:15:36 +0300

    completed third part

--- src/main/java/com/training/java/maven/base/StringCalculator.java
+++ src/main/java/com/training/java/maven/base/StringCalculator.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 
 public class StringCalculator {
     public int Add(String numbers) {
-        if(numbers.isEmpty()) return 0;
+        var sum = 0;
+        if(numbers.isEmpty()) return sum;
 
         if(numbers.contains(",\n"))
             throw new InvalidParameterException();
 
-        var sum = 0;
         var extractedNumbers = Arrays.stream(numbers.split(",|\\n")).map(Integer::parseInt).toArray(Integer[]::new);
 
         for (Integer extractedNumber : extractedNumbers) sum += extractedNumber;
