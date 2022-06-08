cff7800127771893795600f8ed155bfa14673b5b
Esin Gedik <esin.gedik@trendyol.com>
2022-06-08 16:02:34 +0300

    completed the second stage

--- src/main/java/com/training/java/maven/base/StringCalculator.java
+++ src/main/java/com/training/java/maven/base/StringCalculator.java
@@ -6,17 +6,11 @@ public class StringCalculator {
     public int Add(String numbers) {
         if(numbers.isEmpty()) return 0;
 
+        var sum = 0;
         var extractedNumbers = Arrays.stream(numbers.split(",")).map(Integer::parseInt).toArray(Integer[]::new);
 
-        if(extractedNumbers.length == 1)
-            return extractedNumbers[0];
+        for (Integer extractedNumber : extractedNumbers) sum += extractedNumber;
 
-        if(extractedNumbers.length == 2)
-            return extractedNumbers[0] + extractedNumbers[1];
-
-        if(extractedNumbers.length == 4)
-            return extractedNumbers[0] + extractedNumbers[1] + extractedNumbers[2] + extractedNumbers[3];
-
-        return 0;
+        return sum;
     }
 }
